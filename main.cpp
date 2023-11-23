d799952e95f7d0a6998b0af07a664cdc79237f31
nastya <bmixfi0531@gmail.com>
2023-11-23 22:29:21 +0300

    add function

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 using namespace std;
 
+int fibbonachi(int N)
+{
+int ticker = 0;
+int a = 0, b = 1, nextTerm;
+   cout << "Ряд чисел Фибоначчи до " << N << " итераций: ";
+
+   for (int i = 1; i <= N; ++i) {
+        if(ticker==3)
+        {
+        cout << a << " ";
+      ticker =0;
+      }nextTerm = a + b;
+      a = b;
+      b = nextTerm;
+      ticker++;
+   }
+   return 0;
+}
 int main() {
    int N;
    cout << "Введите количество итераций N: ";
    cin >> N;
-
-   int a = 0, b = 1, nextTerm;
-   cout << "Ряд чисел Фибоначчи до " << N << " итераций: ";
-
-   for (int i = 1; i <= N; ++i) {
-   if(i%3==0)
-      cout << a << " ";
-      nextTerm = a + b;
-      a = b;
-      b = nextTerm;
-   }
-   return 0;
+    fibbonachi(N);
 }
