7d1c6c389db89fd5ea08c16df02086e24162e74c
nastya <ksta>
2023-11-23 21:53:23 +0300

    rebuild for only second values

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main() {
    cout << "Ряд чисел Фибоначчи до " << N << " итераций: ";
 
    for (int i = 1; i <= N; ++i) {
+   if(i%2==0)
       cout << a << " ";
       nextTerm = a + b;
       a = b;
